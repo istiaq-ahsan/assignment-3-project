5803002f089e567e800fc54585117ce456e8e636
istiaq-ahsan <ahsanistiaq23@gmail.com>
2024-08-22 17:56:49 +0600

    Almost complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'my-image': "url('/assets/bg.jpg')",
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
